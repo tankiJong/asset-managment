701672c16643fd6f51a90da9e44412d23e41be56
Tanki <tanki_jong@hotmail.com>
2017-01-02 19:35:14 -0600

    remove useless header

--- asset.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Tanki on 2017/1/2.
-//
-
-#ifndef UTIL_ASSET_H
-#define UTIL_ASSET_H
-class Asset;
-#endif //UTIL_ASSET_H
--- list.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Tanki on 2017/1/2.
-//
-
-#ifndef UTIL_LIST_H
-#define UTIL_LIST_H
-extern List;
-#endif //UTIL_LIST_H
